8e2541c19aab41a4028c4aac72859d0416d70048
alajido <96017433+Alajido@users.noreply.github.com>
2022-10-29 15:48:19 +0100

    commit

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
+// require express server
 const express = require('express');
+// use express server
 const app = express();
+// lodash library
 const _ = require('lodash');
+// read and write file module in node server
 const fs = require('fs');
+
+app.set("view engine","ejs");
+app.set("views","./myViews");
 
 //port number
 const PORT = 3000;
 
 // serving static pages
-app.use(express.static('public'));
+app.use(express.static('views'));
 
+// index page route
 app.get('/', (req, res) => {
-    res.sendFile('./public/index.html', {root: __dirname})
+    res.sendFile('./views/index.html', {root: __dirname})
 })
 
-
+// about page route
 app.get('/about', (req, res) => {
-    res.sendFile('./public/about.html', {root: __dirname})
+    res.sendFile('./views/about.html', {root: __dirname})
 })
 
+// redirexting user based on request made to the server
 app.get('/about-us', (req, res) => {
-    res.redirect('/about', '301') 
+    res.redirect('/about', '302');
+    console.log(res.__dirname)
 })
 
+// 404 page
+app.all('*', (req, res) => {
+    res.status(404).sendFile('./views/404.html', {root: __dirname})
+})
+
+// app.use( (res, req) => {
+//     res.status.sendFile('./public/404.html', {root: __dirname})
+// })
+
+// server listing to the port number
 app.listen(PORT, (err, res) => {
     if (err) {
         console.log(err);
