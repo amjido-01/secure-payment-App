3216cb158fbca40106504c555c0b03c23881b228
alajido <96017433+Alajido@users.noreply.github.com>
2022-11-18 17:54:35 +0100

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const passport = require('passport')
+const passport = require('passport');
+const User = require('../model/User.js');
 
 
 router.get('/login', (req, res) => {
@@ -9,7 +10,22 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/register', (req, res) => {
-    res.render('register');
+    const { name, email, password, c_password } = req.body;
+
+    let errors = [];
+    if (!name || !email || !password || !c_password) {
+        errors.push({msg: 'please fill up the inputs!'})
+    };
+
+    // compare the password
+    if (password !== c_password) {
+        errors.push({msg: 'password dont match'})
+    }
+
+    // check if password is less than 6
+    if(password.length < 6) {
+        
+    }
 });
 
 
